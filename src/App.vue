6c00cf38299c18784b99d5a2ed8e98002ae3a06e
LeggyBoiTim <105779117+LeggyBoiTim@users.noreply.github.com>
2025-11-25 14:38:50 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <script setup>
 import { ref, computed } from 'vue';
 
-const people = ref([{ name: 'Jan', age: 12 }, { name: 'Piet', age: 20 }, { name: 'Herman', age: 65 }, { name: 'Ivan', age: 5 }, { name: 'Tom', age: 43 }]);
+const people = ref([
+    { name: 'Jan', age: 12 }, 
+    { name: 'Piet', age: 20 }, 
+    { name: 'Herman', age: 65 }, 
+    { name: 'Ivan', age: 5 }, 
+    { name: 'Tom', age: 43 }
+]);
+
 const newName = ref(null);
 const newAge = ref(null);
 
 const children = computed(() => {
-    return people.value.filter((person) => person.age < 18)
+    return people.value.filter((person) => person.age < 18);
 });
 
 const adults = computed(() => {
-    return people.value.filter((person) => person.age >= 18)
+    return people.value.filter((person) => person.age >= 18);
 });
 
 const totalPeople = computed(() => {
@@ -20,7 +27,6 @@ const totalPeople = computed(() => {
 const numberOfChildren = computed(() => {
     return children.value.length;
 });
-
 
 const addPerson = () => {
     if (newName.value && newAge.value) {
@@ -34,28 +40,33 @@ const addPerson = () => {
 
 
 <template>
-<input v-model="newName" placeholder="Naam" type="text">
-<input v-model.number="newAge" placeholder="Leeftijd" type="number">
+    <input v-model="newName" placeholder="Naam" type="text">
+    <input v-model.number="newAge" placeholder="Leeftijd" type="number">
 
-<button @click="addPerson">Toevoegen</button>
+    <button @click="addPerson">Toevoegen</button>
 
-<ul>
-    <li v-for="(person, index) in people" :key="index">{{ person.name }}, {{ person.age }} jaar oud</li>
-</ul>
+    <ul>
+        <li v-for="(person, index) in people" :key="index">
+            {{ person.name }}, {{ person.age }} jaar oud
+        </li>
+    </ul>
 
-<p>Kinderen:</p>
-<ul>
-    <li v-for="(person, index) in children" :key="index">{{ person.name }}, {{ person.age }} jaar oud</li>
-</ul>
+    <p>Kinderen:</p>
+    <ul>
+        <li v-for="(person, index) in children" :key="index">
+            {{ person.name }}, {{ person.age }} jaar oud
+        </li>
+    </ul>
 
-<p>Volwassenen:</p>
-<ul>
-    <li v-for="(person, index) in adults" :key="index">{{ person.name }}, {{ person.age }} jaar oud</li>
-</ul>
+    <p>Volwassenen:</p>
+    <ul>
+        <li v-for="(person, index) in adults" :key="index">
+            {{ person.name }}, {{ person.age }} jaar oud
+        </li>
+    </ul>
 
-<p>Aantal personen: {{ totalPeople }}</p>
-<p>Aantal kinderen: {{ numberOfChildren }}</p>
-
+    <p>Aantal personen: {{ totalPeople }}</p>
+    <p>Aantal kinderen: {{ numberOfChildren }}</p>
 </template>
 
 
